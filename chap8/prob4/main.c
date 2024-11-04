d3e0006b017856c4c4fc5b73145f01e4d3de97aa
leekangwoo <leekangwoo20@gmail.com>
2024-11-04 02:33:32 +0000

    chap8 prob4 modified

--- chap8/prob4/main.c
+++ chap8/prob4/main.c
@@ -3,10 +3,10 @@
 static void exit_handler1(void), exit_handler2(void);
 
 int main(void){
-	if(atexit(exit_handler1)!=0)
+	if(atexit(exit_handler1) != 0)
 		perror("exit_handler1 등록할 수 없음");
-	if(atexit(exit_handler2)!=0)
-		perror("exit_handler2 드록할 수 없음");
+	if(atexit(exit_handler2) != 0)
+		perror("exit_handler2 등록할 수 없음");
 	printf("main done\n");
 	exit(0);
 }
