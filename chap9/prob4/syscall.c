4b135e39be43013713ae1e2c320a59f768baff25
leekangwoo <leekangwoo20@gmail.com>
2024-11-17 15:21:27 +0000

    chap9 prob4 modify

--- chap9/prob4/syscall.c
+++ chap9/prob4/syscall.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 int main(){
 	int status;
-    if((status = system("date"))<0)
-        perror("system() 오류");
-    printf("종료코드 %d\n", WEXITSTATUS(status));
-    if((status=system("hello"))<0)
-        perror("system() 오류 ");
-    printf("종료코드 %d\n", WEXITSTATUS(status));
-    if((status=system("who; exit 44"))<0)
-		perror("system() 오류 ");
-    printf("종료코드 %d\n", WEXITSTATUS(status));
+	if((status=system("date"))<0)
+		perror("system() 오류");
+	printf("종료코드 %d\n", WEXITSTATUS(status));
+	if((status=system("hello"))<0)
+		perror("system() 오류");
+	printf("종료코드 %d\n", WEXITSTATUS(status));
+	if((status=system("who; exit 44"))<0)
+		perror("system() 오류");
+	printf("종료코드 %d\n", WEXITSTATUS(status));
 }
